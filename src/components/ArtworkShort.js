1cdcb55928264325bb5909977c5df03a510fa733
Sreeja Gupta <sreejagpt@gmail.com>
2017-11-12 14:02:03 +1100

    fix route

--- src/components/ArtworkShort.js
+++ src/components/ArtworkShort.js
@@ -3,10 +3,11 @@ import { Link } from 'react-router-dom';
 
 const ArtworkShort = ({ artwork }) => {
   return (
-    <Link href="/#" to={`/artwork/${artwork.id}`}>
+    <Link href="/#" to={`/artworks/${artwork.id}`}>
       <div className="artwork-short-description">
         <img src={artwork.image} width='20%'></img>
-        <h2>{artwork.artist}</h2>
+        <h2>{artwork.shortTitle}</h2>
+        <h3>{artwork.artist}</h3>
         <p>{artwork.description}</p>
       </div>
     </Link>
