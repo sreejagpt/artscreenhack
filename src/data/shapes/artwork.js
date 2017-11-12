1f4bb65a76fc66a80719f8c951f7f4ff89c93a07
twillzy <william.tanzy@gmail.com>
2017-11-12 12:34:44 +1100

    proptypes for artwork updated

--- src/data/shapes/artwork.js
+++ src/data/shapes/artwork.js
@@ -1,10 +1,11 @@
 import PropTypes from 'prop-types';
 
 export default PropTypes.shape({
+  id: PropTypes.string.isRequired,
   artist: PropTypes.string.isRequired,
   shortTitle: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
   artForm: PropTypes.string.isRequired,
-  startDate: PropTypes.string.isRequired,
-  endDate: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+  personalWebsite: PropTypes.string.isRequired,
 }).isRequired;
