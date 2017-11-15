7d2225c81bc0ebe70339940f321032d24242607d
Sreeja Gupta <sreejagpt@gmail.com>
2017-11-15 14:22:15 +1100

    Fix click behaviour

--- src/components/ArtworkShort.js
+++ src/components/ArtworkShort.js
@@ -5,14 +5,14 @@ import { BASE_URL } from '../url-config';
 const ArtworkShort = ({ artwork }) => {
   const imageUrl = `${BASE_URL}/${artwork.image}`;
   return (
-    <Link href="/#" to={`/artworks/${artwork.id}`}>
-      <div className="artwork-short-description">
+    <div className="artwork-short-description">
+      <Link href="/#" to={`/artworks/${artwork.id}`}>
         <img src={imageUrl} width='20%' alt={artwork.shortTitle}></img>
         <h2>{artwork.shortTitle}</h2>
         <h3>{artwork.artist}</h3>
         <p>{artwork.description}</p>
-      </div>
-    </Link>
+      </Link>
+    </div>
   );
 };
 
